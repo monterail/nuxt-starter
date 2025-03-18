027182082584fc1cda6b00a567f721711435ef23
Baroshem <jakub.andrzejewski.dev@gmail.com>
2025-03-18 11:48:41 +0100

    feat: add Nuxt UI v3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/ui'
+  ],
+  css: ['~/assets/css/main.css'],
+  future: {
+    compatibilityVersion: 4
+  }
 })
