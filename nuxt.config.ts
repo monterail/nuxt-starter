560cad4e7e1bde4fe5dafc7af4581fff5a96a6c4
Baroshem <jakub.andrzejewski.dev@gmail.com>
2025-03-18 12:09:27 +0100

    feat: add VueUse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxt/image'],
+  modules: ['@nuxt/ui', '@nuxt/image', '@vueuse/nuxt'],
   css: ['~/assets/css/main.css'],
   future: {
     compatibilityVersion: 4
